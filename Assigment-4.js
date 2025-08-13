6f84b8fa7a8b353f4aaf2688e3b80ac6fe7ca660
Rakib ullah <rakibullah19730@gmail.com>
2025-08-14 02:48:48 +0600

    3rd task done

--- Assigment-4.js
+++ Assigment-4.js
@@ -28,3 +28,31 @@ function onlyCharacter(str) {
     return charonly.toUpperCase();
 
 }
+
+
+
+// Problem-03 : FIFA Best Team Award 
+function bestTeam(player1, player2) {
+    // You have to write your code here
+    if (typeof player1 !== "object" || typeof player2 !== "object") {
+        return "Invalid";
+    }
+    const team1 = player1.foul + player1.cardR + player1.cardY;
+    const team2 = player2.foul + player2.cardR + player2.cardY;
+    if (team1 < team2) {
+        return player1.name;
+    }
+    else if (team1 > team2) {
+        return player2.name
+    }
+    else {
+        return "Tie"
+    }
+
+}
+// const Germany = { name: "Germany", foul: 12, cardY: 0, cardR: 0 };
+// const Sweden = { name: "Sweden", foul: 7, cardY: 4, cardR: 1 };
+
+
+// const wellplayed = bestTeam(Germany, Sweden);
+// console.log(wellplayed);
